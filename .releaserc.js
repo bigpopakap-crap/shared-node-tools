c786be5d677c5195a46cda645698b70b197a1708
Kapil Easwar <kapil@squareup.com>
2019-10-15 18:52:20 -0700

    fix(autopublish): finally think i might have fixed autopublishing
    addresses #5

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,31 @@
+const hooks = require('semantic-release-monorepo-hooks');
+const output = hooks();
+
 module.exports = {
-  extends: 'semantic-release-monorepo'
+  extends: 'semantic-release-monorepo',
+  branch: 'master',
+  tagFormat: 'v${version}',
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    {
+      'path': '@semantic-release/git',
+      'message': 'chore(' + output.package + '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }
+  ],
+  publish: [
+    '@semantic-release/npm'
+  ],
+  verifyConditions: [
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  monorepo: {
+    analyzeCommits: [
+      '@semantic-release/commit-analyzer'
+    ],
+    generateNotes: [
+      '@semantic-release/release-notes-generator'
+    ]
+  }
 };
